8356944115ffb6d5c04428726ad7bd7e8646bf09
nwkwok <nwkwok@gmail.com>
2020-10-21 23:42:02 -0700

    add jwt logic to register and login

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,27 +2,29 @@ const express = require('express');
 const route = express.Router();
 const pool = require('../db/db');
 const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator')
 
-route.post('/', async (req, res) => {
-    const {email, password} = req.body
-    
+route.post('/', async (req, res) => {    
     try {
+        const {email, password} = req.body
+
         const user = await pool.query(
             'SELECT * FROM users WHERE email = $1',
             [email]
-        );
+        )
 
-        if(user) { 
-            const validatePass = await bcrypt.compare(password, user.rows[0].password)
-            if (validatePass) {
-                res.json('Authenticated User')
-            } else {
-                res.json('Sorry, try again')
-            }
-        } else {
-            res.status(404).json('Sorry, try again');
+        if (user.rows.length === 0) {
+            return res.status(401).json('Passowrd or Email is incorrect');
         }
 
+        const validatePass = await bcrypt.compare(password, user.rows[0].password)
+            if (!validatePass) {
+                res.status(401).json("Passowrd or Email is incorrect");
+            }
+
+        const token = jwtGenerator(user.rows[0].id);
+            res.json({token});
+            
     } catch (err) {
         console.error(err.message)
     }
--- server/routes/register.js
+++ server/routes/register.js
@@ -2,21 +2,35 @@ const express = require('express')
 const route = express.Router();
 const pool = require('../db/db');
 const bcrypt = require('bcrypt')
+const jwtGenerator = require('../utils/jwtGenerator')
 
 // CREATE/REGISTER NEW USER
 route.post('/', async (req, res) => {
     try {
     const {f_name, l_name, email, password} = req.body
+
+    const user = await pool.query(
+        'SELECT * FROM users WHERE email = $1',
+        [email]
+    )
+
+    if(user.rows.length !== 0) {
+        return res.status(401).send('User already exists')
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    const createUser = await pool.query(
+    const newUser = await pool.query(
         'INSERT INTO users (f_name, l_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
         [f_name, l_name, email, hashedPassword]
-    )
+    );
     
-    res.status(200).json(createUser.rows);
+    const token = jwtGenerator(newUser.rows[0].id);
+        res.json({ token });
+
     } catch (err) {
         console.error(err.message);
+        res.status(500).send("Server Error");
     }
 })
 
